4bd6f6440c8a1d79770de75a514ac2f1da511587
AndrewBellamy <andymanbellamy@gmail.com>
2017-05-06 15:24:27 +0930

    Added initial structure for editor and text generator

---
+++ Richly/Richly/RichTextGenerator.swift
@@ -0,0 +1,19 @@
+//
+//  RichTextGenerator.swift
+//  Richly
+//
+//  Created by Andrew Bellamy on 6/5/17.
+//  Copyright © 2017 Andrew Bellamy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class RichTextGenerator {
+    var journal : Journal
+    
+    init(object: Journal) {
+        self.journal = object
+    }
+}
